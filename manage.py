e33fc613d9931994bcee49893f3ca9d28dae58c6
djgoodys <47307908+djgoodys@users.noreply.github.com>
2024-12-19 00:59:14 -0800

    edited a few files

--- manage.py
+++ manage.py
@@ -1,29 +1,10 @@
-#!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 from dotenv import load_dotenv
 
-# Load environment variables from .env file
 load_dotenv()
-
-dir_path = os.path.dirname(os.path.realpath(__file__))
-parent_dir_path = os.path.abspath(os.path.join(dir_path, os.pardir))
-sys.path.insert(0, parent_dir_path)
-
-def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'filtermanagerPython.settings.local')
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
-
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'filtermanagerPython.settings')
 
 if __name__ == '__main__':
-    main()
+    from django.core.management import execute_from_command_line
+    execute_from_command_line(sys.argv)
