0c906670d47dd3176178eb15185aa13b5602bccd
djgoodys <47307908+djgoodys@users.noreply.github.com>
2024-12-19 01:43:31 -0800

    changed wsgi.py adn other files

--- manage.py
+++ manage.py
@@ -7,8 +7,8 @@ root_path = os.path.abspath(os.path.split(__file__)[0])
 sys.path.insert(0, os.path.join(root_path, 'filtermanagerPython'))
 sys.path.insert(0, root_path)
 
-# load_dotenv()
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'filtermanagerPython.settings')
+load_dotenv()
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'filtermanagerPython.settings')
 
 if __name__ == '__main__':
     from django.core.management import execute_from_command_line
